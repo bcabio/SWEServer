ed8e6bf3c08b6488d11379269bac4b8b095c4f22
braxton-mclean <braxton.mclean5@gmail.com>
2018-03-28 22:50:20 -0400

    Added proper post submission logic

--- server.js
+++ server.js
@@ -15,11 +15,27 @@ var bodyParser = require('body-parser');
 mongoose.connect(process.env.MONGO_URL);
 
 var db = mongoose.connection;
+var nextID = 0;
 
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // we're connected!
+  // Find highest id for posts
+  Post.find().sort({id:-1}).limit(1).exec(function(error, posts) {
+    if(error) {
+        console.log("oh no");
+    } else {
+        if (posts == null) {
+            console.log("Oh shitto the database is empty");
+        } else {
+            console.log(nextID);
+            console.log(posts);
+            nextID = posts[0].id + 1;
+            console.log(nextID);
+        }
+    }
+  })
 });
 
 const app = express();
@@ -95,39 +111,36 @@ app.get('/posts', (req, res, next) => {
 });
 
 app.post('/submitPost', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
     console.log(req.body);
     if (req.body.title &&
         req.body.description &&
         req.body.pictureLink) {
 
         var postData = {  
+            id: nextID,
             title: req.body.title,
             description: req.body.description,
             pictureLink: req.body.pictureLink,
+            creator: "TesterDefault"
         }
 
         //use schema.create to insert data into the db
-        User.create(userData, function (err, user) {
+        Post.create(postData, function (err, user) {
             if (err) {
-                return res.send({"response": "The user already exists"});
+                return res.send({"response": "error some how" + err.message});
             } else {
-                console.log('gucii');
-                req.session.userId = user._id;
-                return res.send({"response": "Thank you, " + req.body.username + ", your registration is complete!"});
+                nextID = nextID + 1;
+                return res.send({"response": "Thank you for submitting a post!"});
             }
         });
-    } else if (!req.body.email ||
-                !req.body.username ||
-                !req.body.password || 
-                !req.body.passwordConf) {
+    } else if (!req.body.title ||
+                !req.body.description ||
+                !req.body.pictureLink) {
         var err = new Error('All fields are required');
         err.status = 401;
         return res.send({"response": "All fields are required"});
     } else {
-        return res.send({"response": "The user already exists"})
+        return res.send({"response": "really bad error"})
     }
 });
 
