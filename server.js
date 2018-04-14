35f12ad82c7a5baf55dfed0239cf78222e391012
bcabio <briancabigon@gmail.com>
2018-04-13 21:23:31 -0400

    Fixing cors headers

--- server.js
+++ server.js
@@ -64,12 +64,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname));
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', "https://swe-server.herokuapp.com/");
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  return next();
-});
+// app.use(function(req, res, next) {
+//   // res.setHeader('Access-Control-Allow-Origin', "https://swe-server.herokuapp.com/");
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+//   // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   return next();
+// });
 
 
 app.set('port', (process.env.PORT || 5000));
